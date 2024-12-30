c40089e09448ec79847fa2faeb6bae3a70c495b5
dbuser03 <d.buser@campus.unimib.it>
2024-12-30 20:09:55 +0100

    feat(server): Add backend Spring Boot configuration
    Added a new folder containing all the backend configurations for the Spring Boot application.

---
+++ backend/doit/src/main/java/com/unimib/SingletonSquad/DoIT/DoItApplication.java
@@ -0,0 +1,13 @@
+package com.unimib.SingletonSquad.DoIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoItApplication.class, args);
+	}
+
+}
---
+++ backend/doit/src/test/java/com/unimib/SingletonSquad/DoIT/DoItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unimib.SingletonSquad.DoIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
