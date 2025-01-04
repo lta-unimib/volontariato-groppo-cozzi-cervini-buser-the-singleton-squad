9c3145bcf55b4b166b3667874124a6b6c4119c86
dbuser03 <d.buser@campus.unimib.it>
2025-01-04 13:24:13 +0100

    fix(app): Update Next.js config for manual repository name
    Changelog: fix

--- frontend/doit/next.config.ts
+++ frontend/doit/next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from 'next'
 
+const repositoryName = 'volontariato-groppo-cozzi-cervini-buser-the-singleton-squad';
+
 const nextConfig: NextConfig = {
     output: 'export',
-    assetPrefix: process.env.GITHUB_PAGES ? '/{repository-name}' : '',
-    basePath: process.env.GITHUB_PAGES ? '/{repository-name}' : '',
+    assetPrefix: process.env.GITHUB_PAGES ? `/${repositoryName}` : '',
+    basePath: process.env.GITHUB_PAGES ? `/${repositoryName}` : '',
     images: {
         unoptimized: true,
     },
