8b5ddafb26c6bfdcdcbba33242b8e6088b72e83d
Gabriele Groppo <g.groppo@campus.unimib.it>
2025-01-04 23:04:41 +0100

    feat(server): Add VolunteerCategories
    Categorize Volunteering requests to better filter them

---
+++ backend/doit/src/main/java/com/unimib/singletonsquad/doit/Domain/VolunteerCategories.java
@@ -0,0 +1,9 @@
+package com.unimib.singletonsquad.doit.Domain;
+
+public enum VolunteerCategories {
+    supporto_anziani,
+    supporto_bambini,
+    supporto_disabili,
+    ripetizioni,
+    caritas
+}
