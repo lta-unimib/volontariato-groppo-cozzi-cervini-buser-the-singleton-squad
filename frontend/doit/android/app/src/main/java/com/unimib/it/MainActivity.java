c52d4485e4be8f17ec2bae444039f567a6a57581
dbuser03 <d.buser@campus.unimib.it>
2025-01-04 13:04:58 +0100

    feat(app): Add edge to edge content on mobile app
    Changelog: feature

--- frontend/doit/android/app/src/main/java/com/unimib/it/MainActivity.java
+++ frontend/doit/android/app/src/main/java/com/unimib/it/MainActivity.java
@@ -1,5 +1,19 @@
 package com.unimib.it;
 
+import android.os.Bundle;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+
+        WindowInsetsControllerCompat controller = WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
+        controller.setAppearanceLightStatusBars(true);
+        controller.setAppearanceLightNavigationBars(true);
+    }
+}
