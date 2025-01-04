cd30b55a720d5b01456c8605cd5d190e80a35325
dbuser03 <d.buser@campus.unimib.it>
2025-01-04 13:17:05 +0100

    fix(app): Add basePath to the bg image in home component
    Changelog: fix

--- frontend/doit/src/app/page.tsx
+++ frontend/doit/src/app/page.tsx
@@ -1,6 +1,12 @@
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+
 export default function Home() {
     return (
-        <div className="w-full h-screen bg-cover bg-center" style={{ backgroundImage: 'url(/background.png)' }}>
-        </div>
+        <div
+            className="w-full h-screen bg-cover bg-center"
+            style={{
+                backgroundImage: `url(${basePath}/background.png)`,
+            }}
+        ></div>
     );
 }
