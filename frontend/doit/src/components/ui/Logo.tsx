aeda32cc3836d21474b6202d8b4278399d3e24a4
dbuser03 <d.buser@campus.unimib.it>
2025-01-05 23:54:03 +0100

    test(app): Test basePath compositition without using ternary operator
    Changelog: test

--- frontend/doit/src/components/ui/Logo.tsx
+++ frontend/doit/src/components/ui/Logo.tsx
@@ -1,15 +1,15 @@
 "use client";
-import { useTheme } from "@/hooks/useTheme";
+//import { useTheme } from "@/hooks/useTheme";
 import { ImageWrapper } from '@/components/ui/ImageWrapper';
 
 export function Logo() {
-    const theme = useTheme();
-    const logoSrc = theme === 'dark' ? "/header-logo-dark.svg" : "/header-logo-light.svg";
+    //const theme = useTheme();
+    //const logoSrc = theme === 'dark' ? "/header-logo-dark.svg" : "/header-logo-light.svg";
 
     return (
         <div className="flex items-center justify-center">
             <ImageWrapper
-                src={logoSrc}
+                src="/header-logo-dark.svg"//{logoSrc}
                 alt="Logo"
                 width={200}
                 height={50}
