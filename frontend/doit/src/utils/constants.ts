5c9206e4e74e83af7b1724d2884f6e7d7c46a6bc
dbuser03 <d.buser@campus.unimib.it>
2025-01-05 23:36:57 +0100

    fix(app): Add repository name hardcoded
    Changelog: fix

--- frontend/doit/src/utils/constants.ts
+++ frontend/doit/src/utils/constants.ts
@@ -1,3 +1,3 @@
 export const GITHUB_PAGES = process.env.GITHUB_PAGES === 'true';
-export const REPOSITORY_NAME = process.env.REPOSITORY_NAME ?? '';
+export const REPOSITORY_NAME =  "volontariato-groppo-cozzi-cervini-buser-the-singleton-squad";
 export const BASE_PATH = GITHUB_PAGES ? `/${REPOSITORY_NAME}` : '';
