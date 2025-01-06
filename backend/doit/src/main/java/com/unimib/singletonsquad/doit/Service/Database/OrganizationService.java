5ca513b49a304c2aa67b3435833605b641433397
andrecozzi <a.cozzi25@campus.unimib.it>
2025-01-06 16:52:23 +0100

    fix(server): eliminated duplicate file

--- backend/doit/src/main/java/com/unimib/singletonsquad/doit/Service/Database/OrganizationService.java
+++ backend/doit/src/main/java/com/unimib/singletonsquad/doit/Service/Database/OrganizationService.java
@@ -1,27 +1,33 @@
 package com.unimib.singletonsquad.doit.Service.Database;
 
 import com.unimib.singletonsquad.doit.Domain.Organization;
+import com.unimib.singletonsquad.doit.Domain.Volunteer;
 import com.unimib.singletonsquad.doit.Repository.IOrganizationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
+@Transactional
 public class OrganizationService {
     private final IOrganizationRepository organizationRepository;
 
-
+    @Autowired
     public OrganizationService(IOrganizationRepository organizationRepository) {
         this.organizationRepository = organizationRepository;
     }
 
     public Organization save(Organization organization) {
-        return this.organizationRepository.save(organization);
+        return organizationRepository.save(organization);
+    }
+
+    public Optional<Organization> findOrganizationById(int id) {
+        return organizationRepository.findById((long) id);
     }
 
     public Optional<Organization> findOrganizationByEmail(String email) {
-        return this.organizationRepository.findByEmail(email);
+        return organizationRepository.findByEmail(email);
     }
-
 }
-
