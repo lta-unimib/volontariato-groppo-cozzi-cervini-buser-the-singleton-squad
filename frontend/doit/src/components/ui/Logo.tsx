69058f7e6efdcfe3409428d905e1b2fe8e403c20
dbuser03 <d.buser@campus.unimib.it>
2025-01-06 02:55:43 +0100

    Revert "test(app): Testing Image component in deployment"
    This reverts commit ac7897ea4a2f7f496167ab2dbdc9b57593883cd9.

--- frontend/doit/src/components/ui/Logo.tsx
+++ frontend/doit/src/components/ui/Logo.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useTheme } from "@/hooks/useTheme";
 import Image from "next/image";
-//import { ImageWrapper } from "@/components/ui/ImageWrapper";
+import { ImageWrapper } from "@/components/ui/ImageWrapper";
 import { GITHUB_PAGES } from "@/utils/constants";
 
 export function Logo() {
@@ -12,7 +12,7 @@ export function Logo() {
 
     return (
         <div className="flex items-center justify-center">
-            {/*{GITHUB_PAGES ? (
+            {GITHUB_PAGES ? (
                 <ImageWrapper
                     src="/header-logo-light.svg"
                     alt="Logo"
@@ -26,13 +26,7 @@ export function Logo() {
                     width={200}
                     height={50}
                 />
-            )}*/}
-            <Image
-                src={logoSrc}
-                alt="Logo"
-                width={200}
-                height={50}
-            />
+            )}
         </div>
     );
 }
