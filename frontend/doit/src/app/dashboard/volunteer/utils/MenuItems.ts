c32108c0af39f1a7a495f443bf806c7a6826fa70
Matteo Cervini <m.cervini1@campus.unimib.it>
2025-01-08 01:41:49 +0100

    feat(app): MenuItems for volunteer dashboard has been added

---
+++ frontend/doit/src/app/dashboard/volunteer/utils/MenuItems.ts
@@ -0,0 +1,12 @@
+// MenuItems.ts
+"use client";
+
+import { Inbox} from "lucide-react";
+import { CiViewList } from "react-icons/ci";
+import { CgProfile } from "react-icons/cg";
+
+export const menuItems = [
+    { title: "Opportunità", url: "#", icon: CiViewList },
+    { title: "Messaggi", url: "#", icon: Inbox },
+    { title: "Profilo", url: "#", icon: CgProfile }
+];
