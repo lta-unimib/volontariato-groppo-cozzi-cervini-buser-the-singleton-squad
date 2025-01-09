b5801cb9a881983b7aec82d77eb1659217d2039b
Gabriele Groppo <g.groppo@campus.unimib.it>
2025-01-09 18:32:21 +0100

    fix(server): Fix Location id type
    added builder
    
    Changelog: fix

---
+++ backend/doit/src/test/java/com/unimib/singletonsquad/doit/domain/OrganizationTest.java
@@ -0,0 +1,101 @@
+package com.unimib.singletonsquad.doit.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrganizationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+
+    @Test
+    void setPhoneNumber() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getProfilePicture() {
+    }
+
+    @Test
+    void getSocialNetworks() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void getPhoneNumber() {
+    }
+
+    @Test
+    void getVATNumber() {
+    }
+
+    @Test
+    void getWeSite() {
+    }
+
+    @Test
+    void getOrganizationAddress() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void setDescription() {
+    }
+
+    @Test
+    void setProfilePicture() {
+    }
+
+    @Test
+    void setSocialNetworks() {
+    }
+
+    @Test
+    void setVATNumber() {
+    }
+
+    @Test
+    void setWeSite() {
+    }
+
+    @Test
+    void setOrganizationAddress() {
+    }
+}
