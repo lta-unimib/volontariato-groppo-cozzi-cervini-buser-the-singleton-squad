f8883097aed4f2b973486c0331c8e1891f32f8ce
CerviniMatteo <m.cervini1@campus.unimib.it>
2025-01-09 21:26:20 +0100

    refactor(app): Added to volunteer dashboard a offerCard
    For testing a volunteer dashboard card has been added to volunteer dashboard
    
    Changelog: refactor

--- frontend/doit/src/app/dashboard/volunteer/page.tsx
+++ frontend/doit/src/app/dashboard/volunteer/page.tsx
@@ -1,19 +1,32 @@
 "use client";
 
-import {menuItems} from "@/app/dashboard/volunteer/utils/menuItems";
-import SidebarLayout from "@/components/ui/sidebar/SidebarLayout";
 import {Page} from "@/components/layout/Page";
+import {menuItems} from "@/app/dashboard/organization/utils/menuItems";
+import SidebarLayout from "@/components/ui/sidebar/SidebarLayout";
+import OfferCard from "@/app/dashboard/volunteer/components/OfferCard";
 
 export default function Home() {
     return (
-        <Page>
-            <SidebarLayout menuItems={menuItems} header={""} side={"left"} variant={"floating"}
-                           collapsible={"icon"}>
-                <div>
-                    {/*put here some children content to be displayed side by side with the sidebar*/}
-                </div>
-            </SidebarLayout>
-        </Page>
+        <div className="flex flex-col lg:flex-row w-full">
+            <div>
+                <Page>
+                    <SidebarLayout menuItems={menuItems} header={""} side={"left"} variant={"floating"}
+                                         collapsible={"icon"}>
+                        <div/>
+                    </SidebarLayout>
+                </Page>
+            </div>
 
+            <div className="py-4">
+                <Page>
+                    <OfferCard organization="Gruppo ALBORA"
+                               description="Cerchiamo coordinatrice per: casa GRAZIA"
+                               location="Mori"
+                               date="2/12/2024 - 31/12/2025"
+                               image="https://www.zooplus.it/magazine/wp-content/uploads/2024/01/capibara.jpeg"
+                        />
+                </Page>
+            </div>
+        </div>
     );
 }
