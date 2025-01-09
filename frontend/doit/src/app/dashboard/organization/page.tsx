7aea124210513f4e3da6a2c32dae7d136e9ed17d
CerviniMatteo <m.cervini1@campus.unimib.it>
2025-01-09 02:08:15 +0100

    feat(app): Preliminary organization page version created
    Preliminary organization version created with relatives icons and labels
    
    Changelog: feature

---
+++ frontend/doit/src/app/dashboard/organization/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import {menuItems} from "@/app/dashboard/organization/util/MenuItems";
+import SidebarLayout from "@/app/components/ui/SidebarLayout";
+import {Page} from "@/components/layout/Page";
+
+export default function Home() {
+    return (
+        <Page>
+            <SidebarLayout menuItems={menuItems} header={""} side={"left"} variant={"floating"}
+                           collapsible={"icon"}>
+                <div>
+                    {/*put here some children content to be displayed side by side with the sidebar*/}
+                </div>
+            </SidebarLayout>
+        </Page>
+
+    );
+}
---
+++ frontend/doit/src/app/dashboard/organization/util/MenuItems.ts
@@ -0,0 +1,11 @@
+"use client";
+
+import { MdOutlineInbox } from "react-icons/md";
+import { MdAssignmentAdd } from "react-icons/md";
+import { MdOutlineAccountCircle } from "react-icons/md";
+
+export const menuItems = [
+    { title: "Offerte volontariato", url: "#", icon: MdAssignmentAdd },
+    { title: "Messaggi", url: "#", icon: MdOutlineInbox },
+    { title: "Profilo", url: "#", icon: MdOutlineAccountCircle },
+];
