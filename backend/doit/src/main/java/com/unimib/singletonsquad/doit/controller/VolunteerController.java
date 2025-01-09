2922ed080e3ffd368073f1e282e967886670b8f3
Gabriele Groppo <g.groppo@campus.unimib.it>
2025-01-09 15:04:07 +0100

    fix(server): Fixed the id retrieve from token
    Successfully recognize betweend non registered & registered users
    
    Changelog: fix

--- backend/doit/src/main/java/com/unimib/singletonsquad/doit/controller/VolunteerController.java
+++ backend/doit/src/main/java/com/unimib/singletonsquad/doit/controller/VolunteerController.java
@@ -1,10 +1,12 @@
 package com.unimib.singletonsquad.doit.controller;
 
 import com.unimib.singletonsquad.doit.dto.SignInFormVolunteerDTO;
+import com.unimib.singletonsquad.doit.security.JWTUtils;
 import com.unimib.singletonsquad.doit.service.database.VolunteerService;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,12 +18,25 @@ public class VolunteerController {
 
     @Autowired
     private VolunteerService volunteerService;
+    @Autowired
+    private JWTUtils jwtUtils;
 
     @GetMapping("/registrato")
-    public ResponseEntity<Boolean> isRegistered(@RequestHeader("Authorization") String token) {
+    public ResponseEntity<Boolean> isRegistered(HttpServletRequest request) {
         try {
-            System.out.println(token);
-            return ResponseEntity.ok(volunteerService.isRegistered(1L));
+            System.out.println("isRegistered");
+            String token = request.getSession().getAttribute("token").toString();
+            String id = this.jwtUtils.extractUsername(token);
+            System.out.println(id);
+            //String role = request.getSession().getAttribute("role").toString();
+            //System.out.println(role);
+            //System.out.println(id);
+            if(true) {
+                return ResponseEntity.ok(volunteerService.isRegistered(Long.parseLong(id)));
+            }else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
+
         } catch (ExpiredJwtException e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         } catch (Exception e) {
