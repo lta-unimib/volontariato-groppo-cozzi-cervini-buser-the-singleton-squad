f0b1c3743e269123960ac4622b51f4e4cf9e6a8d
CerviniMatteo <m.cervini1@campus.unimib.it>
2025-01-09 21:24:49 +0100

    feat(app): Create new custom card component
    Created new custom card component for displaying organization offers inside volunteer dashboard
    
    Changelog: feature

--- frontend/doit/src/app/dashboard/volunteer/components/OfferCard.tsx
+++ frontend/doit/src/app/dashboard/volunteer/components/OfferCard.tsx
@@ -1,12 +1,50 @@
 import { Page } from '@/components/layout/Page';
-import { Header } from '@/components/layout/Header';
-import { Hero } from "@/components/ui/Hero";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/Card";
+import React from "react";
 
-export default function Home() {
+interface LayoutProps {
+    organization: string,
+    description: string,
+    location: string,
+    date: string,
+    image: string
+}
+
+export default function OfferCard({
+                                    organization,
+                                    description,
+                                    location,
+                                    date,
+                                    image
+                                  }: LayoutProps) {
     return (
         <Page>
-            <Header />
-            <Hero />
+            <Card className="flex items-stretch gap-4">
+                <div className="flex-1">
+                    <CardHeader>
+                        <CardDescription>{organization}</CardDescription>
+                        <CardTitle>{description}</CardTitle>
+                    </CardHeader>
+
+                    <CardFooter>
+                        <div className="flex flex-col gap-2">
+                            <div className="flex items-center">
+                                <CardDescription>{location}</CardDescription>
+                            </div>
+                            <div className="flex items-center">
+                                <CardDescription>{date}</CardDescription>
+                            </div>
+                        </div>
+                    </CardFooter>
+                </div>
+                <div className="flex-shrink-0">
+                    <img
+                        src={image}
+                        alt="Descrizione immagine"
+                        className="w-60 h-full object-cover rounded-r-md shadow-lg"
+                    />
+                </div>
+            </Card>
         </Page>
     );
 }
