983c1395b1354fb77ac99b922422b0a6dd422abf
CerviniMatteo <m.cervini1@campus.unimib.it>
2025-01-10 16:13:18 +0100

    feat(app): feat: add floating button with navigation to OfferForm
    Added a floating action button in the bottom-right corner for quick navigation to the OfferForm page, using useRouter for seamless routing. Adjusted the layout to include a full-height SidebarLayout while ensuring the button remains in the foreground with responsive design compatibility.
    
    Changelog: feature

--- frontend/doit/src/app/dashboard/organization/page.tsx
+++ frontend/doit/src/app/dashboard/organization/page.tsx
@@ -3,17 +3,40 @@
 import {organizationMenuItems} from "@/utils/organizationMenuItems";
 import SidebarLayout from "@/components/ui/sidebar/SidebarLayout";
 import {Page} from "@/components/layout/Page";
+import {MdOutlineAdd} from "react-icons/md";
+import {Button} from "@/components/ui/Button";
+import {Hero} from "@/components/ui/Hero";
+import {useRouter} from "next/navigation";
 
 export default function Home() {
+    const router = useRouter();
+
     return (
         <Page>
-            <SidebarLayout menuItems={organizationMenuItems} header={""} side={"left"} variant={"floating"}
-                           collapsible={"icon"}>
-                <div>
-                    {/*put here some children content to be displayed side by side with the sidebar*/}
+            <div className="flex-1 my-4">
+                <Button
+                    variant="default"
+                    className="fixed bottom-4 right-4 z-50 p-4 rounded-full h-20 w-20"
+                    size="icon"
+                    onClick={event => router.push("../offer")}
+                >
+                    <MdOutlineAdd/>
+                </Button>
+            </div>
+            <div className="flex w-full h-screen">
+                <div className="w-[var(--sidebar-width)]">
+                    <SidebarLayout
+                        menuItems={organizationMenuItems}
+                        header={""}
+                        side={"left"}
+                        variant={"floating"}
+                        collapsible={"icon"}>
+                        <div>
+                            {/*put here some children content to be displayed side by side with the sidebar*/}
+                        </div>
+                    </SidebarLayout>
                 </div>
-            </SidebarLayout>
+            </div>
         </Page>
-
-    );
+);
 }
