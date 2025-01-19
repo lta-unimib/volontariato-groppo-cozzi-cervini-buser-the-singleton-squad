a4aee26aedeb07d63f25f20fc7e278d5a645ccb6
Gabriele Groppo <g.groppo@campus.unimib.it>
2025-01-19 17:50:40 +0100

    fix(server): Fixed duplicated files generated after merge with dev
    Changelog: fix

--- backend/doit/src/main/java/com/unimib/singletonsquad/doit/repository/IVolunteerOfferRepository.java
+++
@@ -1,7 +0,0 @@
-package com.unimib.singletonsquad.doit.repository;
-
-import com.unimib.singletonsquad.doit.domain.volunteer.VolunteerOffer;
-
-public interface IVolunteerOfferRepository {
-    VolunteerOffer save(VolunteerOffer offer);
-}
--- backend/doit/src/main/java/com/unimib/singletonsquad/doit/repository/JPAAvailabilityRespository.java
+++
@@ -1,8 +0,0 @@
-package com.unimib.singletonsquad.doit.repository;
-
-import com.unimib.singletonsquad.doit.domain.common.Availability;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JPAAvailabilityRespository extends JpaRepository<Availability, Long>, IAvailabilityRepository {
-
-}
