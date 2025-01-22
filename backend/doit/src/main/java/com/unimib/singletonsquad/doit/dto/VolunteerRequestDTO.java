c45ca68ca9ef9cc6a91efc8b23d9ff0c05dfee79
andrecozzi <a.cozzi25@campus.unimib.it>
2025-01-22 12:30:37 +0100

    fix(server): fix VolunteerRequestDTO + datetime method

--- backend/doit/src/main/java/com/unimib/singletonsquad/doit/dto/VolunteerRequestDTO.java
+++ backend/doit/src/main/java/com/unimib/singletonsquad/doit/dto/VolunteerRequestDTO.java
@@ -1,30 +1,73 @@
 package com.unimib.singletonsquad.doit.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Locale;
 
-@Getter
-@Setter
-@ToString
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class VolunteerRequestDTO {
-    @JsonProperty("title")
+    @NotNull
     private String title;
-    @JsonProperty("description")
-    private String detailedDescription;
-    @JsonProperty("address")
+
+    @NotNull
+    private List<String> timeRange;
+
+    @NotNull
     private AddressDTO address;
-    @JsonProperty("startTime")
-    private String startDate;
-    @JsonProperty("endTime")
-    String endDate;
-    @JsonProperty("volunteerCapacity")
-    private int volunteerCapacity;
-    @JsonProperty("categories")
+
+    @NotNull
     private List<String> categories;
-    @JsonProperty("frequency")
-    private List<String> volunteerType;
+
+    @NotNull
+    private List<String> frequency;
+
+    @NotNull
+    private String description;
+
+    @NotNull
+    private Integer volunteerCapacity;
+
+    @NotNull
+    private String startTime;
+
+    @NotNull
+    private String endTime;
+
+
+    // TODO AGGIUNTA METODI PER FARE IL PARSING
+    public static LocalDateTime combineDateTime(String date, String time) {
+        // Parse della data
+        LocalDate localDate = LocalDate.parse(date);
+
+        // Parse del tempo --> obblifatorio english perchè usiamo AM e PM
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("hh:mm a", Locale.ENGLISH);
+        LocalTime localTime = LocalTime.parse(time, timeFormatter);
+
+        // Combina data e tempo
+        return LocalDateTime.of(localDate, localTime);
+    }
+    public void setTimeRangeAndStartTime(List<String> timeRange, String startTime, String endTime) {
+        if (timeRange != null && !timeRange.isEmpty()
+                && startTime != null && !startTime.isEmpty()
+                && endTime != null  && !endTime.isEmpty()) {
+            this.startDate = combineDateTime(timeRange.get(0), startTime);
+            this.endDate = combineDateTime(timeRange.get(1), endTime);
+        }
+    }
+    // Metodo per estrarre data e ora da LocalDateTime nei formati richiesti
+    public static String[] extractDateTime(LocalDateTime dateTime) {
+        String date = dateTime.toLocalDate().toString(); // Formato "yyyy-MM-dd"
+        String time = dateTime.toLocalTime().format(DateTimeFormatter.ofPattern("hh:mm a", Locale.ENGLISH));
+        return new String[]{date, time};
+    }
 }
