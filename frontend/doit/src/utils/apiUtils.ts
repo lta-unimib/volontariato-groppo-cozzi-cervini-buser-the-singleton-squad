a3dd84a23dcbab4b4ba7e2e935f6a8bcd5860fe2
Gabriele Groppo <g.groppo@campus.unimib.it>
2025-01-23 02:09:07 +0100

    feat(app): Add makeDelete function in API Utils
    Function to Make HTTP Delete request
    
    Changelog: feature

--- frontend/doit/src/utils/apiUtils.ts
+++ frontend/doit/src/utils/apiUtils.ts
@@ -11,7 +11,7 @@ interface ApiResponse<T> {
 export async function makePostRequest<T>(
     endpoint: string,
     data?: unknown,
-    options: RequestInit = {}
+    options: RequestInit = {}//TODO REMOVE USE DATA INSTEAD
 ): Promise<ApiResponse<T>> {
     try {
         const token = sessionStorage.getItem('authToken');
@@ -122,6 +122,52 @@ export async function makeUpdateRequest<T>(
             ...options,
             headers,
             body: JSON.stringify(data)
+        });
+
+        const responseData = await response.json();
+        console.log('Response Status:', response.status);
+        console.log('Response Data:', responseData);
+
+        return {
+            status: response.status,
+            data: responseData.data,
+            message: responseData.message
+        };
+    } catch (error) {
+        const errorMessage = error instanceof Error ? error.message : "An unexpected error occurred";
+        console.error('Error occurred:', errorMessage);
+
+        return {
+            status: 500,
+            message: errorMessage
+        };
+    }
+}
+
+export async function makeDeleteRequest<T>(
+    endpoint: string,
+    data?: unknown,
+    options: RequestInit = {}
+): Promise<ApiResponse<T>> {
+    try {
+        const token = sessionStorage.getItem('authToken');
+        console.log('Auth Token:', token);
+
+        const headers: HeadersInit = {
+            'Content-Type': 'application/json',
+            ...(token && { 'Authorization': `Bearer ${token}` }),
+            ...options.headers
+        };
+
+        console.log('Making DELETE request to:', `${API_BASE_LINK}${endpoint}`);
+        console.log('Request Headers:', headers);
+        console.log('Request Body:', JSON.stringify(data));
+
+        const response = await fetch(`${API_BASE_LINK}${endpoint}`, {
+            method: 'DELETE',
+            ...options,
+            headers,
+            body: data ? JSON.stringify(data) : undefined
         });
 
         const responseData = await response.json();
