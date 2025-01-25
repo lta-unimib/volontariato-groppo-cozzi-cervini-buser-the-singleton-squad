35d4961d21a90b6131bdc609305a2cbd7cbef66c
CerviniMatteo <m.cervini1@campus.unimib.it>
2025-01-25 18:08:07 +0100

    refactor(app): addSingleMarkers to use ReadonlyArray for locations
    Refactored the addSingleMarkers function to use ReadonlyArray<google.maps.LatLngLiteral> instead of a mutable array for the locations parameter. This change ensures immutability and improves type safety.
    
    Changelog: refactor

--- frontend/doit/src/hooks/maps/markers.ts
+++ frontend/doit/src/hooks/maps/markers.ts
@@ -2,7 +2,7 @@ export const addSingleMarkers = ({
                                      locations,
                                      map,
                                  }: {
-    locations: google.maps.LatLngLiteral[];
+    locations: ReadonlyArray<google.maps.LatLngLiteral>;
     map: google.maps.Map;
 }) => {
     locations.forEach((location) => {
